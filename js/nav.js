e939dd7cf3088d8bd321c921273c1bbce02f7488
tai096 <trinhtai09@gmail.com>
2023-03-07 15:22:06 +0700

    fix darkmode

--- js/nav.js
+++ js/nav.js
@@ -1,6 +1,6 @@
 const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".nav__menu");
-const checkbox = document.getElementById("checkbox");
+const ball = document.getElementById("ball");
 
 hamburger.addEventListener("click", () => {
   hamburger.classList.toggle("active");
@@ -21,6 +21,26 @@ document.querySelectorAll("div.toggle").forEach((item) =>
   })
 );
 
-checkbox.addEventListener("change", () => {
-  document.body.classList.toggle("dark");
-});
+const handleDarkMode = () => {
+  const setTheme = document.body;
+  let theme = "lightMode";
+
+  setTheme.classList.toggle("darkmode");
+
+  if (setTheme.classList.contains("darkmode")) {
+    ball.style.transform = "translate(39px)";
+    theme = "darkMode";
+  } else {
+    ball.style.transform = "translate(0px)";
+    theme = "lightMode";
+  }
+
+  localStorage.setItem("webTheme", JSON.stringify(theme));
+};
+
+let getTheme = JSON.parse(localStorage.getItem("webTheme"));
+
+if (getTheme === "darkMode") {
+  document.body.classList = "darkmode";
+  ball.style.transform = "translate(39px)";
+}
