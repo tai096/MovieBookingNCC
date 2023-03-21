8a732d174bee49229d5c1d4041c44fa4f03f8380
tai096 <trinhtaii09@gmail.com>
2023-03-21 23:13:41 +0700

    hotfix slider, add button

--- js/theme.js
+++ js/theme.js
@@ -1,5 +1,6 @@
 const ball = document.getElementById("ball");
 const footer = document.getElementById("footer");
+const controlBtns = document.querySelectorAll(".home__control_btn");
 
 const handleDarkMode = () => {
   const setTheme = document.body;
@@ -8,13 +9,19 @@ const handleDarkMode = () => {
   setTheme.classList.toggle("darkmode");
 
   if (setTheme.classList.contains("darkmode")) {
+    theme = "darkMode";
     ball.style.transform = "translate(39px)";
     footer.style.background = "transparent";
-    theme = "darkMode";
+    controlBtns.forEach((controlBtn) => {
+      controlBtn.style.color = "white";
+    });
   } else {
+    theme = "lightMode";
     ball.style.transform = "translate(0px)";
     footer.style.background = "var(--darkBrown)";
-    theme = "lightMode";
+    controlBtns.forEach((controlBtn) => {
+      controlBtn.style.color = "black";
+    });
   }
 
   localStorage.setItem("webTheme", JSON.stringify(theme));
@@ -26,4 +33,7 @@ if (getTheme === "darkMode") {
   document.body.classList = "darkmode";
   ball.style.transform = "translate(39px)";
   footer.style.background = "transparent";
+  controlBtns.forEach((controlBtn) => {
+    controlBtn.style.color = "white";
+  });
 }
