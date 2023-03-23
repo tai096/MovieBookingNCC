4f9ae4aa821862896d88b24fdfc5e287c69d871b
tai096 <trinhtai09@gmail.com>
2023-03-23 17:34:35 +0700

    temp commit

--- js/home.js
+++ js/home.js
@@ -1,7 +1,13 @@
-const sliders = document.querySelector(".carouselBox");
-var scrollPerClick = 1000;
+const sliders = document.querySelector(".home__movies_carousel-list");
+const switchLeft = document.getElementById("switchLeft");
+const switchRight = document.getElementById("switchRight");
+
+var scrollPerClick = 500;
 
 var scrollAmount = 0;
+
+let showSwitchLeft = "none";
+switchLeft.style.display = showSwitchLeft;
 
 const sliderScrollLeft = () => {
   sliders.scrollTo({
@@ -9,18 +15,26 @@ const sliderScrollLeft = () => {
     left: (scrollAmount -= scrollPerClick),
     behavior: "smooth",
   });
+  switchRight.style.display = "block";
 
-  if (scrollAmount < 0) {
+  if (scrollAmount <= 0) {
     scrollAmount = 0;
+    showSwitchLeft = "none";
+    switchLeft.style.display = showSwitchLeft;
   }
 };
 
 const sliderScrollRight = () => {
+  console.log(scrollAmount);
   if (scrollAmount <= sliders.scrollWidth - sliders.clientWidth) {
     sliders.scrollTo({
       top: 0,
       left: (scrollAmount += scrollPerClick),
       behavior: "smooth",
     });
+    showSwitchLeft = "block";
+    switchLeft.style.display = showSwitchLeft;
+  } else {
+    switchRight.style.display = "none";
   }
 };
