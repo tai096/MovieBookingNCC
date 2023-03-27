b1b2569020f9dd6558eaf02f64cdc479ff35abf3
tai096 <trinhtai09@gmail.com>
2023-03-27 15:19:42 +0700

    deploy web on firebase

---
+++ public/services/fakeData.js
@@ -0,0 +1,547 @@
+export const movies = [
+  {
+    name: "NGƯỜI KIẾN VÀ CHIẾN  BINH ONG: Thế giới lượng tử ",
+    summary:
+      "After the devastating events of Avengers: Infinity War, the universe is in ruins. With the help of remaining allies, the Avengers assemble once more in order to undo Thanos' actions and restore order to the universe.",
+    image: "../assets/img/antman.jpeg",
+    director: "Anthony Russo, Joe Russo",
+    cast: "Robert Downey Jr, Chris Evans",
+    genre: "Action, Adventure, Sci-Fi",
+    duration: "120p",
+    release_date: "17/02/2023",
+    type: "nowPlaying",
+    schedule: [
+      {
+        date: "2023-03-15",
+        showtimes: [
+          {
+            start_time: "10:00",
+            end_time: "12:01",
+          },
+          {
+            start_time: "12:30",
+            end_time: "14:31",
+          },
+        ],
+      },
+      {
+        date: "2023-03-16",
+        showtimes: [
+          {
+            start_time: "11:00",
+            end_time: "13:01",
+          },
+          {
+            start_time: "13:30",
+            end_time: "15:31",
+          },
+        ],
+      },
+    ],
+  },
+  {
+    name: "Mèo Béo Siêu Đẳng",
+    type: "nowPlaying",
+    summary:
+      "When the menace known as the Joker wreaks havoc and chaos on the people of Gotham, Batman must accept one of the greatest psychological and physical tests of his ability to fight injustice.",
+    image: "../assets/img/meobeo.jpeg",
+    director: "Christopher Nolan",
+    cast: "Christian Bale, Heath Ledger",
+    genre: "Action, Crime, Drama",
+    duration: "100m",
+    release_date: "24/02/2023",
+    schedule: [
+      {
+        date: "2023-03-16",
+        showtimes: [
+          {
+            start_time: "11:30",
+            end_time: "13:32",
+          },
+          {
+            start_time: "14:00",
+            end_time: "16:02",
+          },
+          {
+            start_time: "18:30",
+            end_time: "20:32",
+          },
+        ],
+      },
+      {
+        date: "2023-03-17",
+        showtimes: [
+          {
+            start_time: "11:30",
+            end_time: "13:32",
+          },
+          {
+            start_time: "14:00",
+            end_time: "16:02",
+          },
+          {
+            start_time: "18:30",
+            end_time: "20:32",
+          },
+        ],
+      },
+    ],
+  },
+  {
+    name: "Khóa Chặt Cửa Nào Suzume",
+    type: "nowPlaying",
+    summary:
+      "When the menace known as the Joker wreaks havoc and chaos on the people of Gotham, Batman must accept one of the greatest psychological and physical tests of his ability to fight injustice.",
+    image: "../assets/img/suzume.jpeg",
+    director: "Christopher Nolan",
+    cast: "Christian Bale, Heath Ledger",
+    genre: "Action, Crime, Drama",
+    duration: "100m",
+    release_date: "24/02/2023",
+    schedule: [
+      {
+        date: "2023-03-16",
+        showtimes: [
+          {
+            start_time: "11:30",
+            end_time: "13:32",
+          },
+          {
+            start_time: "14:00",
+            end_time: "16:02",
+          },
+          {
+            start_time: "18:30",
+            end_time: "20:32",
+          },
+        ],
+      },
+      {
+        date: "2023-03-17",
+        showtimes: [
+          {
+            start_time: "11:30",
+            end_time: "13:32",
+          },
+          {
+            start_time: "14:00",
+            end_time: "16:02",
+          },
+          {
+            start_time: "18:30",
+            end_time: "20:32",
+          },
+        ],
+      },
+    ],
+  },
+  {
+    name: "Thanh Gươm Diệt Quỷ",
+    type: "nowPlaying",
+    summary:
+      "When the menace known as the Joker wreaks havoc and chaos on the people of Gotham, Batman must accept one of the greatest psychological and physical tests of his ability to fight injustice.",
+    image: "../assets/img/thanhguom.jpeg",
+    director: "Christopher Nolan",
+    cast: "Christian Bale, Heath Ledger",
+    genre: "Action, Crime, Drama",
+    duration: "100m",
+    release_date: "24/02/2023",
+    schedule: [
+      {
+        date: "2023-03-16",
+        showtimes: [
+          {
+            start_time: "11:30",
+            end_time: "13:32",
+          },
+          {
+            start_time: "14:00",
+            end_time: "16:02",
+          },
+          {
+            start_time: "18:30",
+            end_time: "20:32",
+          },
+        ],
+      },
+      {
+        date: "2023-03-17",
+        showtimes: [
+          {
+            start_time: "11:30",
+            end_time: "13:32",
+          },
+          {
+            start_time: "14:00",
+            end_time: "16:02",
+          },
+          {
+            start_time: "18:30",
+            end_time: "20:32",
+          },
+        ],
+      },
+    ],
+  },
+  {
+    name: "Titanic",
+    type: "nowPlaying",
+    summary:
+      "When the menace known as the Joker wreaks havoc and chaos on the people of Gotham, Batman must accept one of the greatest psychological and physical tests of his ability to fight injustice.",
+    image: "../assets/img/titanic.jpeg",
+    director: "Christopher Nolan",
+    cast: "Christian Bale, Heath Ledger",
+    genre: "Action, Crime, Drama",
+    duration: "100m",
+    release_date: "24/02/2023",
+    schedule: [
+      {
+        date: "2023-03-16",
+        showtimes: [
+          {
+            start_time: "11:30",
+            end_time: "13:32",
+          },
+          {
+            start_time: "14:00",
+            end_time: "16:02",
+          },
+          {
+            start_time: "18:30",
+            end_time: "20:32",
+          },
+        ],
+      },
+      {
+        date: "2023-03-17",
+        showtimes: [
+          {
+            start_time: "11:30",
+            end_time: "13:32",
+          },
+          {
+            start_time: "14:00",
+            end_time: "16:02",
+          },
+          {
+            start_time: "18:30",
+            end_time: "20:32",
+          },
+        ],
+      },
+    ],
+  },
+  {
+    name: "Shazam 2",
+    type: "nowPlaying",
+    summary:
+      "When the menace known as the Joker wreaks havoc and chaos on the people of Gotham, Batman must accept one of the greatest psychological and physical tests of his ability to fight injustice.",
+    image: "../assets/img/shazam.jpeg",
+    director: "Christopher Nolan",
+    cast: "Christian Bale, Heath Ledger",
+    genre: "Action, Crime, Drama",
+    duration: "100m",
+    release_date: "24/02/2023",
+    schedule: [
+      {
+        date: "2023-03-16",
+        showtimes: [
+          {
+            start_time: "11:30",
+            end_time: "13:32",
+          },
+          {
+            start_time: "14:00",
+            end_time: "16:02",
+          },
+          {
+            start_time: "18:30",
+            end_time: "20:32",
+          },
+        ],
+      },
+      {
+        date: "2023-03-17",
+        showtimes: [
+          {
+            start_time: "11:30",
+            end_time: "13:32",
+          },
+          {
+            start_time: "14:00",
+            end_time: "16:02",
+          },
+          {
+            start_time: "18:30",
+            end_time: "20:32",
+          },
+        ],
+      },
+    ],
+  },
+  {
+    name: "SIÊU LỪA GẶP SIÊU LẦY",
+    summary:
+      "After the devastating events of Avengers: Infinity War, the universe is in ruins. With the help of remaining allies, the Avengers assemble once more in order to undo Thanos' actions and restore order to the universe.",
+    image: "../assets/img/sieulua.jpeg",
+    director: "Anthony Russo, Joe Russo",
+    cast: "Robert Downey Jr, Chris Evans",
+    genre: "Action, Adventure, Sci-Fi",
+    duration: "85m",
+    release_date: "03/03/2023",
+    type: "comingSoon",
+    schedule: [
+      {
+        date: "2023-03-15",
+        showtimes: [
+          {
+            start_time: "10:00",
+            end_time: "12:01",
+          },
+          {
+            start_time: "12:30",
+            end_time: "14:31",
+          },
+        ],
+      },
+      {
+        date: "2023-03-16",
+        showtimes: [
+          {
+            start_time: "11:00",
+            end_time: "13:01",
+          },
+          {
+            start_time: "13:30",
+            end_time: "15:31",
+          },
+        ],
+      },
+      {
+        date: "2023-03-17",
+        showtimes: [
+          {
+            start_time: "11:00",
+            end_time: "13:01",
+          },
+          {
+            start_time: "13:30",
+            end_time: "15:31",
+          },
+        ],
+      },
+    ],
+  },
+  {
+    name: "Nghi Thức Trừ Tà",
+    summary:
+      "After the devastating events of Avengers: Infinity War, the universe is in ruins. With the help of remaining allies, the Avengers assemble once more in order to undo Thanos' actions and restore order to the universe.",
+    image: "../assets/img/truta.jpeg",
+    director: "Anthony Russo, Joe Russo",
+    cast: "Robert Downey Jr, Chris Evans",
+    genre: "Action, Adventure, Sci-Fi",
+    duration: "85m",
+    release_date: "03/03/2023",
+    type: "comingSoon",
+    schedule: [
+      {
+        date: "2023-03-15",
+        showtimes: [
+          {
+            start_time: "10:00",
+            end_time: "12:01",
+          },
+          {
+            start_time: "12:30",
+            end_time: "14:31",
+          },
+        ],
+      },
+      {
+        date: "2023-03-16",
+        showtimes: [
+          {
+            start_time: "11:00",
+            end_time: "13:01",
+          },
+          {
+            start_time: "13:30",
+            end_time: "15:31",
+          },
+        ],
+      },
+      {
+        date: "2023-03-17",
+        showtimes: [
+          {
+            start_time: "11:00",
+            end_time: "13:01",
+          },
+          {
+            start_time: "13:30",
+            end_time: "15:31",
+          },
+        ],
+      },
+    ],
+  },
+  {
+    name: "Muốn Gặp Anh",
+    summary:
+      "After the devastating events of Avengers: Infinity War, the universe is in ruins. With the help of remaining allies, the Avengers assemble once more in order to undo Thanos' actions and restore order to the universe.",
+    image: "../assets/img/muongapanh.jpeg",
+    director: "Anthony Russo, Joe Russo",
+    cast: "Robert Downey Jr, Chris Evans",
+    genre: "Action, Adventure, Sci-Fi",
+    duration: "85m",
+    release_date: "03/03/2023",
+    type: "comingSoon",
+    schedule: [
+      {
+        date: "2023-03-15",
+        showtimes: [
+          {
+            start_time: "10:00",
+            end_time: "12:01",
+          },
+          {
+            start_time: "12:30",
+            end_time: "14:31",
+          },
+        ],
+      },
+      {
+        date: "2023-03-16",
+        showtimes: [
+          {
+            start_time: "11:00",
+            end_time: "13:01",
+          },
+          {
+            start_time: "13:30",
+            end_time: "15:31",
+          },
+        ],
+      },
+      {
+        date: "2023-03-17",
+        showtimes: [
+          {
+            start_time: "11:00",
+            end_time: "13:01",
+          },
+          {
+            start_time: "13:30",
+            end_time: "15:31",
+          },
+        ],
+      },
+    ],
+  },
+  {
+    name: "Tri Kỷ",
+    summary:
+      "After the devastating events of Avengers: Infinity War, the universe is in ruins. With the help of remaining allies, the Avengers assemble once more in order to undo Thanos' actions and restore order to the universe.",
+    image: "../assets/img/triky.jpeg",
+    director: "Anthony Russo, Joe Russo",
+    cast: "Robert Downey Jr, Chris Evans",
+    genre: "Action, Adventure, Sci-Fi",
+    duration: "85m",
+    release_date: "03/03/2023",
+    type: "comingSoon",
+    schedule: [
+      {
+        date: "2023-03-15",
+        showtimes: [
+          {
+            start_time: "10:00",
+            end_time: "12:01",
+          },
+          {
+            start_time: "12:30",
+            end_time: "14:31",
+          },
+        ],
+      },
+      {
+        date: "2023-03-16",
+        showtimes: [
+          {
+            start_time: "11:00",
+            end_time: "13:01",
+          },
+          {
+            start_time: "13:30",
+            end_time: "15:31",
+          },
+        ],
+      },
+      {
+        date: "2023-03-17",
+        showtimes: [
+          {
+            start_time: "11:00",
+            end_time: "13:01",
+          },
+          {
+            start_time: "13:30",
+            end_time: "15:31",
+          },
+        ],
+      },
+    ],
+  },
+  {
+    name: "Creed 3",
+    summary:
+      "After the devastating events of Avengers: Infinity War, the universe is in ruins. With the help of remaining allies, the Avengers assemble once more in order to undo Thanos' actions and restore order to the universe.",
+    image: "../assets/img/creed.jpeg",
+    director: "Anthony Russo, Joe Russo",
+    cast: "Robert Downey Jr, Chris Evans",
+    genre: "Action, Adventure, Sci-Fi",
+    duration: "85m",
+    release_date: "03/03/2023",
+    type: "comingSoon",
+    schedule: [
+      {
+        date: "2023-03-15",
+        showtimes: [
+          {
+            start_time: "10:00",
+            end_time: "12:01",
+          },
+          {
+            start_time: "12:30",
+            end_time: "14:31",
+          },
+        ],
+      },
+      {
+        date: "2023-03-16",
+        showtimes: [
+          {
+            start_time: "11:00",
+            end_time: "13:01",
+          },
+          {
+            start_time: "13:30",
+            end_time: "15:31",
+          },
+        ],
+      },
+      {
+        date: "2023-03-17",
+        showtimes: [
+          {
+            start_time: "11:00",
+            end_time: "13:01",
+          },
+          {
+            start_time: "13:30",
+            end_time: "15:31",
+          },
+        ],
+      },
+    ],
+  },
+];
