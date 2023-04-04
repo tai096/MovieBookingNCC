fc01a1468e3aac67921516a66387d037e4bd3923
tai096 <trinhtaii09@gmail.com>
2023-04-04 11:31:16 +0700

    temp navMobie

--- js/nav.js
+++ js/nav.js
@@ -1,6 +1,14 @@
 const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".nav__mobie");
 
+const navHome = document.getElementById("navHome");
+const navIntroduction = document.getElementById("navIntroduction");
+const navShowtimes = document.getElementById("navShowtimes");
+const navNews = document.getElementById("navNews");
+const navPrice = document.getElementById("navPrice");
+
+const windowLocation = window.location.pathname;
+
 hamburger.addEventListener("click", () => {
   hamburger.classList.toggle("active");
   navMenu.classList.toggle("active");
@@ -19,3 +27,28 @@ document.querySelectorAll("div.toggle").forEach((item) =>
     navMenu.classList.remove("active");
   })
 );
+
+if (windowLocation == "/index.html") {
+  navHome.style.borderBottom = "2px solid var(--secondaryRed)";
+  navHome.style.color = "white";
+}
+
+if (windowLocation == "/introduction.html") {
+  navIntroduction.style.borderBottom = "2px solid var(--secondaryRed)";
+  navIntroduction.style.color = "white";
+}
+
+if (windowLocation == "/showtimes.html") {
+  navShowtimes.style.borderBottom = "2px solid var(--secondaryRed)";
+  navShowtimes.style.color = "white";
+}
+
+if (windowLocation == "/news.html") {
+  navNews.style.borderBottom = "2px solid var(--secondaryRed)";
+  navNews.style.color = "white";
+}
+
+if (windowLocation == "/price.html") {
+  navPrice.style.borderBottom = "2px solid var(--secondaryRed)";
+  navPrice.style.color = "white";
+}
