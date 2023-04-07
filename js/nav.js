ddd25c9a4c7b7b65deabbb940a99e97bc430b32b
tai096 <trinhtai09@gmail.com>
2023-04-07 15:22:55 +0700

    temp commit

--- js/nav.js
+++ js/nav.js
@@ -72,7 +72,9 @@ var prevScrollpos = window.pageYOffset;
 
 window.onscroll = function () {
   var currentScrollPos = window.pageYOffset;
-  if (prevScrollpos > currentScrollPos) {
+  if (currentScrollPos == 0) {
+    document.getElementById("navbar").style.top = "0";
+  } else if (prevScrollpos > currentScrollPos) {
     document.getElementById("navbar").style.top = "0";
   } else {
     document.getElementById("navbar").style.top = "-100px";
